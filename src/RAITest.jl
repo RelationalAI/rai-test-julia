0601550cbd5fafa40d9a73bb89cc40979dd68b03
Martin McGrane <mmcgr@users.noreply.github.com>
2022-11-28 12:26:38 +1100

    Engine pool refactor (#17)
    * Refactor default engine pool into separate file
    
    * Export new engine creation setter

--- src/RAITest.jl
+++ src/RAITest.jl
@@ -7,13 +7,15 @@ export Step
 export ConcurrentTestSet
 export destroy_test_engines, resize_test_engine_pool, provision_all_test_engines, add_test_engine!
 export set_context
-export set_engine_name_provider, set_engine_name_releaser
+export set_engine_name_provider, set_engine_name_releaser, set_engine_creater
 
 include("testsets.jl")
 
 include("code-util.jl")
 
 include("testrel.jl")
+
+include("testpool.jl")
 
 include("engines.jl")
 
