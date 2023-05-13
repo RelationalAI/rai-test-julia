cc1d542d2b8e7ae2c714442e27427032c4f46a13
Mary McGrath <m.c.mcgrath13@gmail.com>
2023-05-13 08:41:23 -0400

    fix: check flakiness

--- src/testrel.jl
+++ src/testrel.jl
@@ -417,7 +417,7 @@ function _test_rel_steps(;
 
     try
         stats = @timed Logging.with_logger(logger) do
-            @testset TestRelTestSet nested=nested "$(string(name))" begin
+            @testset TestRelTestSet nested=nested name begin
                 create_test_database(schema, clone_db)
                 for (index, step) in enumerate(steps)
                     _test_rel_step(index, step, schema, test_engine, name, length(steps))
@@ -426,6 +426,8 @@ function _test_rel_steps(;
         end
         duration = sprint(show, stats.time; context=:compact => true)
         ts = stats.value
+        
+        check_flaky(name, logger.logs)
 
         if anyerror(ts) || anyfail(ts)
             io, ctx = get_logging_io()
@@ -467,6 +469,19 @@ function _test_rel_steps(;
             @warn("Could not delete test database: ", schema)
         end
         user_engine === nothing && release_test_engine(test_engine)
+    end
+end
+
+function check_flaky(name::String, logs::Vector{LogRecord})
+    retries = 0
+    for log in logger.logs
+        if haskey(log.kwargs, :submit_failed) && log.kwargs[:retry_number] > retries
+            retries = log.kwargs[:retry_number]
+        end
+    end
+
+    if retries > 0
+        @warn "[FLAKY] $name: transaction submission had to be retried $retries times"
     end
 end
 
@@ -518,20 +533,20 @@ function _execute_test(
     program::String,
     timeout_sec::Int64,
     readonly::Bool;
-    num_retries=0,
+    retry_number=1,
 )
     @debug("$name: Starting execution")
     rsp = nothing
     txn_id = nothing
     try
         # Exec async really should return after 2-3 seconds
+        throw("uh oh")
         rsp = exec_async(context, schema, engine, program; readtimeout=30, readonly)
         txn_id = rsp.transaction.id
         @info "$name: Executing with txn $txn_id" transaction_id = txn_id
-        throw("uh oh")
     catch e
-        @error "$name: Failed to submit transaction\n\n$e"
-        if num_retries < 3
+        @error "$name: Failed to submit transaction\n\n$e" retry_number submit_failed=true
+        if retry_number <= 3
             # Try again
             return _execute_test(
                 name,
@@ -541,7 +556,7 @@ function _execute_test(
                 program,
                 timeout_sec,
                 readonly;
-                num_retries=num_retries + 1,
+                retry_number=retry_number + 1,
             )
         end
         rethrow()
