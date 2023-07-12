e0e56f19553fc797d6246464485e324c28d8d4b5
Martin McGrane <martin.mcgrane@relational.ai>
2023-07-12 12:15:41 +1000

    Give tired systems more time to sleep

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,22 +7,22 @@ for distributed in (true, false)
     ts = @testset RAITestSet "outer" distributed=distributed begin
         @testset "middle" begin
             @testset TestRelTestSet "inner" begin
-                @test (sleep(1); true)
+                @test (sleep(2); true)
             end
         end
     end
     @testset "record (distributed=$distributed)" begin
         outer = ts.dts
         @test outer.description == "outer"
-        @test 1 < outer.time_end - outer.time_start < 2
+        @test 2 < outer.time_end - outer.time_start < 3
         @test length(outer.results) == 1
         middle = only(outer.results)
         @test middle.description == "middle"
-        @test 1 < middle.time_end - middle.time_start < 2
+        @test 2 < middle.time_end - middle.time_start < 3
         @test length(middle.results) == 1
         inner = only(middle.results)
         @test inner.description == "inner"
-        @test 1 < inner.time_end - inner.time_start < 2
+        @test 2 < inner.time_end - inner.time_start < 3
         @test isempty(inner.results)
         @test inner.n_passed == 1
     end
