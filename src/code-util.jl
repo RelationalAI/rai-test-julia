7c6144da0dd4945ac81c6a34fd3729e37a88bbcc
Martin McGrane <martin.mcgrane@relational.ai>
2024-04-24 10:44:52 +1000

    CoreRel migrations

--- src/code-util.jl
+++ src/code-util.jl
@@ -47,16 +47,11 @@ function convert_input_dict_to_string(inputs::AbstractDict)
     for input in inputs
         name = string(input.first)
 
-        program *= "\ndef insert:" * name * " = "
+        program *= "\ndef insert[:" * name * "] { "
 
         first = true
 
         values = input.second
-
-        if isempty(values)
-            program *= "{ }"
-            continue
-        end
 
         values = to_vector_of_tuples(values)
 
@@ -69,6 +64,7 @@ function convert_input_dict_to_string(inputs::AbstractDict)
 
             program *= input_element_to_string(i)
         end
+        program *= " }"
     end
     return program
 end
@@ -111,7 +107,7 @@ function generate_output_string_from_expected(expected::AbstractDict)
         is_special_symbol(e.first) && continue
 
         name = string(e.first)
-        program *= "\ndef output:" * name * " = " * name
+        program *= "\ndef output[:" * name * "]: " * name
     end
     return program
 end
--- test/integration.jl
+++ test/integration.jl
@@ -4,21 +4,20 @@
 @test_rel(
     name = "No input, expected, or output",
     query = """
-    def result = 1
-    """,
-)
+    def result { 1 }
+    """,)
 
 @test_rel(
     name = "No input or expected, has output",
     query = """
-    def output = 1
+    def output { 1 }
     """,
 )
 
 @test_rel(
     name = "Input, no expected or output",
     query = """
-    def result = input
+    def result { input }
     """,
     inputs = Dict(:input => [1]),
 )
@@ -26,7 +25,7 @@
 @test_rel(
     name = "Input, output, no expected",
     query = """
-    def output = input
+    def output { input }
     """,
     inputs = Dict(:input => [1]),
 )
@@ -34,7 +33,7 @@
 @test_rel(
     name = "No input, expected output",
     query = """
-    def output = 1
+    def output { 1 }
     """,
     expected = Dict(:output => [1]),
 )
@@ -42,7 +41,7 @@
 @test_rel(
     name = "Input, expected output",
     query = """
-    def output = input
+    def output { input }
     """,
     inputs = Dict(:input => [1]),
     expected = Dict(:output => [1]),
@@ -51,7 +50,7 @@
 @test_rel(
     name = "Empty expected, empty actual",
     query = """
-    def output = false
+    def output { false }
     """,
     expected = Dict(:output => []),
 )
@@ -59,7 +58,7 @@
 @test_rel(
     name = "Empty expected, present actual",
     query = """
-    def output = true
+    def output { true }
     """,
     expected = Dict(:output => []),
     broken = true,
@@ -68,7 +67,7 @@
 @test_rel(
     name = "Broken expected",
     query = """
-    def output = 1
+    def output { 1 }
     """,
     expected = Dict(:output => [2]),
     broken = true,
@@ -77,8 +76,8 @@
 @test_rel(
     name = "Expected abort",
     query = """
-    def result = 1
-    ic { result = 2 }
+    def result { 1 }
+    ic () requires result = 2
     """,
     expect_abort = true,
 )
@@ -86,8 +85,8 @@
 @test_rel(
     name = "Broken abort",
     query = """
-    def result = 1
-    ic { result = 2 }
+    def result { 1 }
+    ic () requires result = 2
     """,
     broken = true,
 )
@@ -95,26 +94,26 @@
 @test_rel(
     name = "Expected problem",
     query = """
-    def output = a
+    def output { a }
     """,
-    expected_problems = [(:code => :UNDEFINED, :line => 1)],
+    expected_problems = [(:code => :UNDEFINED_IDENTIFIER, :line => 1)],
 )
 
 @test_rel(
     name = "Expected problem, allow all",
     query = """
-    def output = a
+    def output { a }
     """,
-    expected_problems = [(:code => :UNDEFINED, :line => 1)],
+    expected_problems = [(:code => :UNDEFINED_IDENTIFIER, :line => 1)],
     allow_unexpected = :error,
 )
 
 @test_rel(
     name = "Expected problem, allow none",
     query = """
-    def output = a
+    def output { a }
     """,
-    expected_problems = [(:code => :UNDEFINED, :line => 1)],
+    expected_problems = [(:code => :UNDEFINED_IDENTIFIER, :line => 1)],
     allow_unexpected = :none,
 )
 
@@ -122,19 +121,20 @@
     name = "Expected problems, allow none",
     query = """
     // Line 1
-    def output = a
-    def output = b
+    def output { a }
+    def output { b }
     """,
     expected_problems = [
-        (:code => :UNDEFINED, :line => 2),
-        (:code => :UNDEFINED, :line => 3)],
+        (:code => :UNDEFINED_IDENTIFIER, :line => 2),
+        (:code => :UNDEFINED_IDENTIFIER, :line => 3),
+    ],
     allow_unexpected = :none,
 )
 
 @test_rel(
     name = "Unexpected problem, ignore all",
     query = """
-    def output = a
+    def output { a }
     """,
     allow_unexpected = :error,
 )
@@ -142,14 +142,14 @@
 @test_rel(
     name = "Unexpected problem, broken",
     query = """
-    def output = a
+    def output { a }
     """,
     broken = true,
 )
 
 @test_rel(
     name = "abort_on_error",
-    query = "def output = a",
+    query = "def output { a }",
     abort_on_error = true,
     expect_abort = true,
 )
@@ -157,23 +157,15 @@
 # `setup` and `tags` keywords ignored
 # --------------------------------------------------------
 
-@test_rel(
-    name = "tags keyword ignored",
-    query = "def result = 1",
-    tags=[:foo],
-)
+@test_rel(name = "tags keyword ignored", query = "def result { 1 }", tags = [:foo],)
 
 module FooSetup end
 
-@test_rel(
-    name = "setup keyword ignored",
-    query = "def result = 1",
-    setup=FooSetup,
-)
+@test_rel(name = "setup keyword ignored", query = "def result { 1 }", setup = FooSetup,)
 
 @test_rel(
     name = "setup and tags keywords ignored",
-    query = "def result = 1",
-    setup=FooSetup,
-    tags=[:foo],
+    query = "def result { 1 }",
+    setup = FooSetup,
+    tags = [:foo],
 )
